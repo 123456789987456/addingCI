ba3d967d1b4a0adfabe432ffc0e05dec7be15c31
123456789987456 <94908302+123456789987456@users.noreply.github.com>
2022-04-05 08:00:03 -0400

    Add files via upload

---
+++ addingCI.py
@@ -0,0 +1,5 @@
+print("This is an adding machine")
+first_number = int(input("what is the first number you want to add"))
+second_number = int(input("what is the second number you want to add"))
+sum = first_number + second_number
+print("the sumof your two numbers is", sum)
