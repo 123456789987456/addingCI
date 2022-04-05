97828a49bdd8e7518299fe1da19a3ef8b5332dde
123456789987456 <94908302+123456789987456@users.noreply.github.com>
2022-04-05 08:00:13 -0400

    Delete addingCI.py

--- addingCI.py
+++
@@ -1,5 +0,0 @@
-print("This is an adding machine")
-first_number = int(input("what is the first number you want to add"))
-second_number = int(input("what is the second number you want to add"))
-sum = first_number + second_number
-print("the sumof your two numbers is", sum)
